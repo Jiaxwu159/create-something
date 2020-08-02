d4b49a32853f74df3455d94b79ecd3b6abbc4f18
Jiaxi <dev.jiaxiwu@gmail.com>
2020-08-02 14:43:17 -0400

    added fireworks css

---
+++ create-something/src/components/Fireworks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+    render() {
+        function Fireworks(props) {
+            
+        }
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
